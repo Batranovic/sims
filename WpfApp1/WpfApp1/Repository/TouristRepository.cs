a7ef0ee760e934db6fe642056c1631fee82c7768
Nina Batranovic <batranovic.ra48.2020@uns.ac.rs>
2023-03-14 17:20:21 +0100

    Tourist

---
+++ WpfApp1/WpfApp1/Repository/TouristRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+using WpfApp1.Serializer;
+
+namespace WpfApp1.Repository
+{
+    public class TouristRepository : IRepository<Tourist>
+    {
+        private const string _filePath = "../../../Resources/Data/tourists.csv";
+
+        private readonly Serializer<Tourist> _serializer;
+
+        private List<Tourist> _tourists;
+
+        public TouristRepository()
+        {
+            _serializer = new Serializer<Tourist>();
+            _tourists = new List<Tourist>();
+            _tourists = _serializer.FromCSV(_filePath);
+        }
+        public Tourist Get(int id)
+        {
+            return _tourists.Find(t => t.Id == id);
+        }
+
+        public List<Tourist> GetAll()
+        {
+            return _tourists;
+        }
+    }
+}
--- WpfApp1/WpfApp1/View/TourView.xaml.cs
+++ WpfApp1/WpfApp1/View/TourView.xaml.cs
@@ -22,30 +22,116 @@ namespace WpfApp1.View
     /// <summary>
     /// Interaction logic for TourView.xaml
     /// </summary>
-    public partial class TourView : Window
+    public partial class TourView : Window, INotifyPropertyChanged
+
     {
+        public event PropertyChangedEventHandler PropertyChanged;
         public LocationController LocationController { get; set; }
 
+        public ObservableCollection<Tour> Tours { get; set; }
         public TourController TourController { get; set; }
 
         public TourView()
         {
             InitializeComponent();
-            this.DataContext = this;
+            DataContext = this;
 
             var app = Application.Current as App;
             LocationController = app.LocationController;
             
+            
         }
+
+        private string _city;
+
+        public string City
+        {
+            get => _city;
+
+            set
+            {
+                if (value != _city)
+                {
+                    _city = value;
+                    OnPropertyChanged("City");
+                }
+            }
+        }
+
+        private string _state;
+
+        public string State
+        {
+            get => _state;
+
+            set
+            {
+                if (value != _state)
+                {
+                    _state = value;
+                    OnPropertyChanged("State");
+                }
+            }
+        }
+
+        private TimeSpan _duration;
+        public TimeSpan Duration
+        {
+            get => _duration;
+
+            set
+            {
+                if (value != _duration)
+                {
+                    _duration = value;
+                    OnPropertyChanged("Duration");
+                }
+            }
+        }
+
+        private string _language;
+
+        public string Language
+        {
+            get => _language;
+
+            set
+            {
+                if (value != _language)
+                {
+                    _language = value;
+                    OnPropertyChanged("Language");
+                }
+            }
+        }
+
+        private int _maxGuests;
+        public int MaxGuests
+        {
+            get => _maxGuests;
+            set
+            {
+                if (_maxGuests != value)
+                {
+                    _maxGuests = value;
+                    OnPropertyChanged("MaxGuests");
+                }
+            }
+        }
+    
+
+
 
         private void Search(object sender, RoutedEventArgs e)
         {
-
+           // Tours = new ObservableCollection<Tour>(TourController.Search(Location, Duration, Language, MaxGuests));
         }
 
-        private void Restart(object sender, RoutedEventArgs e)
+        protected void OnPropertyChanged(string propertyName)
         {
-
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+
     }
 }
