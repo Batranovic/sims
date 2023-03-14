29ba738b1ccc616571bab327b8d74f8230f98d10
Nina Batranovic <batranovic.ra48.2020@uns.ac.rs>
2023-03-14 14:37:06 +0100

    Tour, TourDAO, TourController

---
+++ WpfApp1/WpfApp1/View/TourView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.Model.Enums;
+using WpfApp1.Model;
+using System.Collections.ObjectModel;
+using WpfApp1.Controller;
+using System.ComponentModel;
+
+namespace WpfApp1.View
+{
+    /// <summary>
+    /// Interaction logic for TourView.xaml
+    /// </summary>
+    public partial class TourView : Window
+    {
+        public LocationController LocationController { get; set; }
+
+        public TourController TourController { get; set; }
+
+        public TourView()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            var app = Application.Current as App;
+            LocationController = app.LocationController;
+            
+        }
+
+        private void Search(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Restart(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
