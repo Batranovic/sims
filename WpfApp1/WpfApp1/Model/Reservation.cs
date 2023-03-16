3583041317731c1ff1edd21f4beffd6ae4c66798
Koldan01 <koldan.ra232.2020@uns.ac.rs>
2023-03-16 17:34:33 +0100

    Added ReservationDAO, ReservationController and Reservation(Model)

---
+++ WpfApp1/WpfApp1/Model/Reservation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using WpfApp1.Repository;
+using WpfApp1.Serializer;
+using WpfApp1.Util;
+
+namespace WpfApp1.Model
+{
+    public  class Reservation : WpfApp1.Serializer.ISerializable
+    {
+        public int Id { get; set; } 
+        public Accommodation Accommodation { get; set; }
+
+        public int IdAccommodation { get; set; }
+        public DateTime StartDate { get; set; } 
+
+        public DateTime EndDate { get; set; }   
+
+        public Reservation()
+        {
+            
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+                Id.ToString(),
+                DateHelper.DateToString(StartDate),
+                DateHelper.DateToString(EndDate),
+                IdAccommodation.ToString()
+            };
+            return csvValues;
+        }
+
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            StartDate = DateHelper.StringToDate(values[1]);
+            EndDate = DateHelper.StringToDate(values[2]);
+            IdAccommodation = Convert.ToInt32(values[3]);
+        }
+
+    }
+}
---
+++ WpfApp1/WpfApp1/Repository/ReservationDAO.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp.Observer;
+using WpfApp1.Model;
+using WpfApp1.Serializer;
+
+namespace WpfApp1.Repository
+{
+    public class ReservationDAO : IDAO<Reservation>, ISubject
+    {
+
+        private const string _filePath = "../../../Resources/data/reservations.csv";
+        private readonly List<IObserver> _observers;
+
+        private readonly Serializer<Reservation> _serializer;
+
+        private List<Reservation> _reservations;
+
+        public ReservationDAO()
+        {
+            _reservations = new List<Reservation>();
+            _serializer= new Serializer<Reservation>();
+            _reservations = _serializer.FromCSV(_filePath);
+            _observers = new List<IObserver>();
+
+        }
+        public Reservation Create(Reservation entity)
+        {
+            entity.Id = NextId();
+            _reservations.Add(entity);
+            Save();
+            NotifyObservers();
+            return entity;
+        }
+
+        public Reservation Delete(Reservation entity)
+        {
+            _reservations.Remove(entity);
+            Save();
+            NotifyObservers();
+            return entity;
+        }
+
+        public Reservation Get(int id)
+        {
+            return _reservations.Find(l => l.Id == id);
+        }
+
+        public List<Reservation> GetAll()
+        {
+            return _reservations;
+        }
+
+        public int NextId()
+        {
+            if(_reservations.Count == 0)
+                return 0;
+            int nextId = _reservations[_reservations.Count - 1].Id + 1;
+            foreach(Reservation r in _reservations)
+            {
+                if(nextId == r.Id)
+                {
+                    nextId++;
+                }
+            }
+            return nextId;
+        }
+
+        public void NotifyObservers()
+        {
+            foreach(var observer in _observers)
+            {
+                observer.Update();
+            }
+        }
+
+        public void Save()
+        {
+            _serializer.ToCSV(_filePath, _reservations);
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Unsubscribe(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public Reservation Update(Reservation entity)
+        {
+            var oldEntity = Get(entity.Id);
+            if(oldEntity == null)
+            {
+                return null;
+            }
+            oldEntity = entity;
+            Save();
+            NotifyObservers();
+            return oldEntity;
+
+        }
+    }
+}
