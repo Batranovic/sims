9001b0f045fedb4ef2e757e7d4a0901b92358e8a
Koldan01 <koldan.ra232.2020@uns.ac.rs>
2023-05-29 22:07:53 +0200

    Something

---
+++ WpfApp1/WpfApp1/Domain/ServiceInterfaces/IForumService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp.Observer;
+using WpfApp1.Domain.Models;
+using WpfApp1.Domain.ServiceInterfaces;
+
+namespace WpfApp1.Service
+{
+    public interface IForumService : IService<Forum>
+    {
+        void Create(Forum entity);
+        void Delete(Forum entity);
+        void Subscribe(IObserver observer);
+        void Unsubscribe(IObserver observer);
+    }
+}
